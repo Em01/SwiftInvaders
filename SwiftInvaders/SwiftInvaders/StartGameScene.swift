ce34604acf988483167d9da205aa84f809fe4bf2
Em01 <emma_williams1@hotmail.co.uk>
2015-04-23 18:10:40 +0100

    further work on start game scene

--- SwiftInvaders/SwiftInvaders/StartGameScene.swift
+++ SwiftInvaders/SwiftInvaders/StartGameScene.swift
@@ -16,5 +16,21 @@ class StartGameScene: SKScene {
         startGameButton.name = "startgame"
         addChild(startGameButton)
     }
+
+
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        for touch: AnyObject in touches {
+            let touchLocation = touch.locationInNode(self)
+            let touchedNode = self.nodeAtPoint(touchLocation)
+            if(touchedNode.name == "startgame"){
+                let gameOverScene = GameScene(size: size)
+                gameOverScene.scaleMode = scaleMode
+                let transitionType = SKTransition.flipHorizontalWithDuration(1.0)
+                view?.presentScene(gameOverScene,transition: transitionType)
+            }
+        }
+    }
 }
+
+
 
