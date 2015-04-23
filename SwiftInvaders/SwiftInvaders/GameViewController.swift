6fbb426642fd83257743c3735fc892d82d1d56c3
Em01 <emma_williams1@hotmail.co.uk>
2015-04-23 17:37:58 +0100

    initial commit

---
+++ SwiftInvaders/SwiftInvaders/GameViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GameViewController.swift
+//  SwiftInvaders
+//
+//  Created by emma williams on 23/04/2015.
+//  Copyright (c) 2015 Emma Williams. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let skView = view as SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
